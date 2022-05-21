64904bd629d11b82ed81019ef76292ab9e3677ad
Eduardo Romero <eduardoromeror9@outlook.com>
2022-05-21 07:11:08 -0400

    Mundial de pronosticos dia 22

--- main.py
+++ main.py
@@ -1,37 +1,38 @@
 from prettytable import PrettyTable
 
 legend = """
-      🔥🔥 INFORMACION IMPORTANTE 🔥🔥
-      Don Eduardo Romero,
-      mejor conocido como 
-      el KING de los PRONOSTICOS
-      ES EL VIGENTE CAMPEON
-             HASTA QUE 
-      SE DEMUESTRE LO CONTRARIO
+  🔥🔥 INFORMACION IMPORTANTE 🔥🔥
+  Don Eduardo Romero,
+  mejor conocido como 
+  el KING de los PRONOSTICOS
+  ES EL VIGENTE CAMPEON
+         HASTA QUE 
+  SE DEMUESTRE LO CONTRARIO
   \n"""
 
 jornadas = """  
-          Jornadas: 25
-          Juegos: 75
+          Jornadas: 26
+          Juegos: 78
 """
 
 info = """
+       Christian debe 1
 """
 
 title = PrettyTable(['🔥🔥 Mundial de pronosticos 🔥🔥'])
 tabla_dia = PrettyTable(['Players', 'G-P', 'DIF', 'PCT %'])
 
-tabla_dia.add_row([' Eduardo',   '45-30', '-', ' 600 '])
-tabla_dia.add_row([' Erycherd',  '44-31', '1', ' 585 '])
-tabla_dia.add_row([' CarlosJ',   '44-31', '1', ' 585 '])
-tabla_dia.add_row([' Christian', '43-32', '2', ' 575 '])
-tabla_dia.add_row([' Kleydi',    '42-33', '3', ' 560 '])
-tabla_dia.add_row([' Steven',    '41-34', '4', ' 545 '])
-tabla_dia.add_row([' Daniel',    '41-34', '4', ' 545 '])
+tabla_dia.add_row([' Eduardo',   '47-31', '-', ' 605 '])
+tabla_dia.add_row([' Kleydi',    '45-33', '2', ' 575 '])
+tabla_dia.add_row([' Erycherd',  '45-33', '2', ' 575 '])
+tabla_dia.add_row([' Christian', '45-32', '2', ' 575 '])
+tabla_dia.add_row([' CarlosJ',   '45-33', '2', ' 575 '])
+tabla_dia.add_row([' Steven',    '43-35', '4', ' 550 '])
+tabla_dia.add_row([' Daniel',    '43-35', '4', ' 550 '])
 
 
 print('\n',jornadas)
-# print(info)
+print(info)
 print('\n   ⚾⚾⚾⚾ Tabla del DIA ⚾⚾⚾⚾\n')
 print(title)
 print(tabla_dia,'\n')
