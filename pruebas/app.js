68085695c67caa3748c0b0e137549394ea9cc514
Eduardo Romero <eduardoromeror9@outlook.com>
2022-09-26 04:10:22 -0300

    Mundial de pronosticos dia 92, ahora con node

--- pruebas/app.js
+++ pruebas/app.js
@@ -22,13 +22,13 @@ function table(input) {
 }
 
 const test = [
-  { Pos: 1 ,Nombre: "Steven",    Juegos: 309, Ganados: 188, Perdidos: 121,  Dif: '-', Porcentaje: 610, diasPerfectos: 10},
-  { Pos: 2 ,Nombre: "Eduardo",   Juegos: 309, Ganados: 187, Perdidos: 122,  Dif:  1,  Porcentaje: 605, diasPerfectos: 8},
-  { Pos: 3 ,Nombre: "CarlosJ",   Juegos: 309, Ganados: 185, Perdidos: 124,  Dif:  3,  Porcentaje: 595, diasPerfectos: 9165458},
-  { Pos: 4 ,Nombre: "Kleydi",    Juegos: 309, Ganados: 180, Perdidos: 129,  Dif:  8,  Porcentaje: 580, diasPerfectos: 9},
-  { Pos: 5 ,Nombre: "Daniel",    Juegos: 309, Ganados: 174, Perdidos: 135,  Dif:  14, Porcentaje: 565, diasPerfectos: 1},
-  { Pos: 6 ,Nombre: "Erycherd",  Juegos: 309, Ganados: 171, Perdidos: 138,  Dif:  17, Porcentaje: 555, diasPerfectos: 5},
-  { Pos: 7 ,Nombre: "Christian", Juegos: 309, Ganados: 169, Perdidos: 140,  Dif:  19, Porcentaje: 545, diasPerfectos: 17},
+  { Pos: 1 ,Nombre: "Eduardo",   Juegos: 312, Ganados: 189, Perdidos: 123,  Dif: '-',  Porcentaje: 605, diasPerfectos: 8},
+  { Pos: 2 ,Nombre: "Steven",    Juegos: 312, Ganados: 189, Perdidos: 123,  Dif: '-', Porcentaje: 610, diasPerfectos: 10},
+  { Pos: 3 ,Nombre: "CarlosJ",   Juegos: 312, Ganados: 187, Perdidos: 125,  Dif:  2,  Porcentaje: 595, diasPerfectos: 9165458},
+  { Pos: 4 ,Nombre: "Kleydi",    Juegos: 312, Ganados: 180, Perdidos: 132,  Dif:  9,  Porcentaje: 580, diasPerfectos: 9},
+  { Pos: 5 ,Nombre: "Daniel",    Juegos: 312, Ganados: 175, Perdidos: 137,  Dif:  14, Porcentaje: 565, diasPerfectos: 1},
+  { Pos: 6 ,Nombre: "Erycherd",  Juegos: 312, Ganados: 172, Perdidos: 140,  Dif:  17, Porcentaje: 555, diasPerfectos: 5},
+  { Pos: 7 ,Nombre: "Christian", Juegos: 312, Ganados: 171, Perdidos: 141,  Dif:  19, Porcentaje: 545, diasPerfectos: 17},
 ];
 
 let legend = 
@@ -46,9 +46,9 @@ let legend =
 let info =`
                          🧊🧊🧊🧊🧊🧊🧊🧊🧊🧊🧊🧊🧊🧊🧊🧊🧊🧊
                          🧊                                🧊
-                         🧊      NO Huele a Back To Back   🧊
-                         🧊      NO Huele a Back To Back   🧊  
-                         🧊      NO Huele a Back To Back   🧊  
+                         🧊   CUIDADO con el Back To Back  🧊
+                         🧊   CUIDADO con el Back To Back  🧊  
+                         🧊   CUIDADO con el Back To Back  🧊  
                          🧊                                🧊
                          🧊🧊🧊🧊🧊🧊🧊🧊🧊🧊🧊🧊🧊🧊🧊🧊🧊🧊 \n`
 
