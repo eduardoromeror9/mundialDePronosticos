ecb9382d3245cd5cb792ce6e197c71ff2162891f
Eduardo Romero <eduardoromeror9@outlook.com>
2022-09-28 05:04:03 -0300

    Mundial de pronosticos dia 94, ahora con node

--- pruebas/app.js
+++ pruebas/app.js
@@ -22,13 +22,13 @@ function table(input) {
 }
 
 const test = [
-  { Pos: 1 ,Nombre: "Eduardo",   Juegos: 312, Ganados: 189, Perdidos: 123,  Dif: '-',  Porcentaje: 605, diasPerfectos: 8},
-  { Pos: 2 ,Nombre: "Steven",    Juegos: 312, Ganados: 189, Perdidos: 123,  Dif: '-', Porcentaje: 610, diasPerfectos: 10},
-  { Pos: 3 ,Nombre: "CarlosJ",   Juegos: 312, Ganados: 187, Perdidos: 125,  Dif:  2,  Porcentaje: 595, diasPerfectos: 9165458},
-  { Pos: 4 ,Nombre: "Kleydi",    Juegos: 312, Ganados: 180, Perdidos: 132,  Dif:  9,  Porcentaje: 580, diasPerfectos: 9},
-  { Pos: 5 ,Nombre: "Daniel",    Juegos: 312, Ganados: 175, Perdidos: 137,  Dif:  14, Porcentaje: 565, diasPerfectos: 1},
-  { Pos: 6 ,Nombre: "Erycherd",  Juegos: 312, Ganados: 172, Perdidos: 140,  Dif:  17, Porcentaje: 555, diasPerfectos: 5},
-  { Pos: 7 ,Nombre: "Christian", Juegos: 312, Ganados: 171, Perdidos: 141,  Dif:  19, Porcentaje: 545, diasPerfectos: 17},
+  { Pos: 1 ,Nombre: "Eduardo",   Juegos: 322, Ganados: 196, Perdidos: 126,  Dif: '-', Porcentaje: 610, diasPerfectos: 8},
+  { Pos: 2 ,Nombre: "Steven",    Juegos: 322, Ganados: 194, Perdidos: 128,  Dif:  2,  Porcentaje: 600, diasPerfectos: 10},
+  { Pos: 3 ,Nombre: "CarlosJ",   Juegos: 322, Ganados: 192, Perdidos: 130,  Dif:  4,  Porcentaje: 595, diasPerfectos: 91},
+  { Pos: 4 ,Nombre: "Kleydi",    Juegos: 322, Ganados: 186, Perdidos: 136,  Dif:  10, Porcentaje: 575, diasPerfectos: 9},
+  { Pos: 5 ,Nombre: "Daniel",    Juegos: 322, Ganados: 180, Perdidos: 142,  Dif:  16, Porcentaje: 560, diasPerfectos: 1},
+  { Pos: 6 ,Nombre: "Erycherd",  Juegos: 322, Ganados: 178, Perdidos: 144,  Dif:  18, Porcentaje: 550, diasPerfectos: 5},
+  { Pos: 7 ,Nombre: "Christian", Juegos: 322, Ganados: 178, Perdidos: 144,  Dif:  18, Porcentaje: 550, diasPerfectos: 17},
 ];
 
 let legend = 
